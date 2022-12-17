12319322b771e51934251129a4855c39b5e79478
Zoxidov <108032060+Zoxidov@users.noreply.github.com>
2022-12-17 19:54:15 +0300

    Sunnatilla Header qismini tugatti

--- assets/main/app.js
+++ assets/main/app.js
@@ -1,4 +1,16 @@
 /* Sunnat start */
+$(window).scroll(function () {
+    $('header').each(function () {
+        if ($(this).offset().top < $(window).scrollTop()) {
+            $('.nav').css('opacity', '1')
+        }
+        setTimeout(function () {
+            $(".nav").css('opacity', '0');
+        }, 5000);
+    })
+
+
+})
 
 /* Sunnat end */
 /* Husniddin start */
