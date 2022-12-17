99ed2344fd3f0a17a11da5aa69eb6786a7a164cd
ZoxidovSunnatilla <sunnatzoxidov3@gmail.com>
2022-12-17 10:32:50 +0500

    our first website

---
+++ assets/main/app.js
@@ -0,0 +1,15 @@
+/* Sunnat start */
+
+/* Sunnat end */
+/* Husniddin start */
+
+/* Husniddin end */
+/* Azam start */
+
+/* Azam end */
+/* Abbos start */
+
+/* Abbos end */
+/* Dilnoza start */
+
+/* Dilnoza end */
